6b366e969b1e78c9f05c94c24df4a49a1ab74a16
Fernanda Ribeiro <fernandafcrk@gmail.com>
2023-09-02 02:44:22 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Hoje dou início a um projeto pessoal. Ficou curioso(a)? Me acompanhe nessa jornada.</h1>
+    return <h1>Dei início a um projeto pessoal. Ficou curioso(a)? Me acompanhe nessa jornada.</h1>
 }
 
 export default Home;
