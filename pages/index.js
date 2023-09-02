b9521fbc9457170e874580689aa300ad838c9c81
Fernanda Ribeiro <fernandafcrk@gmail.com>
2023-09-02 21:24:24 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Dei início a um projeto pessoal. Ficou curioso(a)? Me acompanhe nessa jornada.</h1>
+function Home() {
+  return (
+    <h1>
+      Dei início a um projeto pessoal. Ficou curioso(a)? Me acompanhe nessa
+      jornada.
+    </h1>
+  );
 }
 
 export default Home;
