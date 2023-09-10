93f9857bf776ed7aa567e54a0bf580cfc042edda
Fernanda Ribeiro <fernandafcrk@gmail.com>
2023-09-10 01:09:10 +0000

    Observação sobre o projeto.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 function Home() {
   return (
-    <h1>
-      Dei início a um projeto pessoal. Ficou curioso(a)? Me acompanhe nessa
-      jornada.
-    </h1>
+    <>
+      <h1>
+        Dei início a um projeto pessoal. Ficou curioso(a)? Me acompanhe nessa
+        jornada.
+      </h1>
+      <p>
+        A ideia do meu projeto pessoal já foi desenvolvida por alguém e tem até
+        um site em português. Ao navegar por esse site, notei que posso
+        adicionar novas ideias ao meu projeto. Ainda estou em dúvida se devo
+        continuar esse projeto pessoal ou não. O domínio que criei tabviz.com.br
+        está indiretamente relacionado ao projeto.
+      </p>
+    </>
   );
 }
 
